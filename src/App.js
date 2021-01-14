cd9595221cdf25cdc625ed942f9849e1b30a15cb
valentinbiz <valentin.bizzz@gmail.com>
2021-01-14 13:07:03 +0000

    created a colection page and a shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './Pages/Homepage/HomepageComponent';
+import ShopPage from './Pages/ShopPage/ShopComponent';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+    </Switch>
     </div>
   );
 }
