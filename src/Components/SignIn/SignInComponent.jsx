27dc6058ad3fb26418d627f138b5d5d1ee0e0e80
valentinbiz <valentin.bizzz@gmail.com>
2021-01-15 18:21:39 +0000

    finished sign-in sign-up methods

--- src/Components/SignIn/SignInComponent.jsx
+++ src/Components/SignIn/SignInComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../FormInput/FormInputComponent';
 import CustomButton from '../CustomButton/CustomButtonComponent';
 
-import {signInWithGoogle} from '../../Firebase/Firebase.utils';
+import {auth, signInWithGoogle} from '../../Firebase/Firebase.utils';
 import './SignInStyle.scss';
 
 class SignIn extends React.Component {
@@ -16,10 +16,19 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({email: '', password: ''})
+        const {email, password} = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''})
+        } catch(error) {
+            console.error();
+        }
+
+        
     }
 
     handleChange = event => {
